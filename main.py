a502f5926754a7fbf516aa5fd4da02d90d19bcae
Dominik <cogentdom@gmail.com>
2022-02-24 01:56:24 -0800

    ...

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+app = Flask(__name__)
+from datetime import date
+import yfinance as yf
+import pandas as pd
+
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+stocks = ("AAPL", "GOOG", "MSFT", "TSLA")
+
+
+@app.route('/')
+def index():
+    return '<h1>Utility to Retrieve Stock Data</h1>'
+
+@app.route('/price/<ticker>')
+def get_price(ticker):
+    df = yf.download(ticker, START, TODAY)
+    df.reset_index(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date']).dt.date
+    return df.to_json()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
