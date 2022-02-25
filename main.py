ee6c8e9ec02b67ab6603d10c3e6ec0cef2a902bb
Dominik <cogentdom@gmail.com>
2022-02-24 21:10:07 -0800

    Updated route price/<ticker> to return closing price, rolling mean and rolling std dev.

--- main.py
+++ main.py
@@ -18,8 +18,18 @@ def index():
 def get_price(ticker):
     df = yf.download(ticker, START, TODAY)
     df.reset_index(inplace=True)
-    df['Date'] = pd.to_datetime(df['Date']).dt.date
-    return df.to_json()
+    df['Date'] = pd.to_datetime(df['Date'])
+    df.set_index('Date', inplace=True)
+    df = df.resample('2w').mean()
+    
+    chart1 = df['Close']
+    chart2 = df['Close'].rolling(window = 12).mean().dropna()
+    chart3 = df['Close'].rolling(window = 12).std().dropna()
+
+    df_con = pd.concat([chart1, chart2, chart3], axis=1)
+    df_con.columns=['Close', 'Rolling Mean', 'Rolling Std']
+    df_con.reset_index(inplace=True)
+    return df_con.to_json()
 
 
 if __name__ == "__main__":
